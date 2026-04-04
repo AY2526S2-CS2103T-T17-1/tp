a75996bdd8f3221fff35a0731b23e2eb4d3b60a9
Adib Shifas <shifasadib@gmail.com>
2026-04-04 16:56:25 +0800

    Fix trailing whitespace and Checkstyle line limits

--- src/test/java/seedu/address/model/person/PhoneTest.java
+++ src/test/java/seedu/address/model/person/PhoneTest.java
@@ -30,7 +30,7 @@ public class PhoneTest {
         assertFalse(Phone.isValidPhone("91")); // less than 3 numbers
         assertFalse(Phone.isValidPhone("phone")); // non-numeric
         assertFalse(Phone.isValidPhone("9011p041")); // alphabets within digits
-        
+
         // valid phone numbers
         assertTrue(Phone.isValidPhone("9312 1534")); // spaces within digits
 
